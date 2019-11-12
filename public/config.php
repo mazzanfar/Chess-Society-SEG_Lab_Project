6c63a1026770efd5a7afccfe230c9233d1ab3dcb
Fareed Faisal <fareed.faisal@kcl.ac.uk>
2019-11-12 16:47:49 +0000

    Do login system

---
+++ public/config.php
@@ -0,0 +1,31 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+
+/*
+CREATE TABLE users (
+    ->
+    ->     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ->
+    ->     username VARCHAR(50) NOT NULL UNIQUE,
+    ->
+    ->     password VARCHAR(255) NOT NULL,
+    ->
+    ->     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    ->
+    -> );
+ */
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'chess_society_db');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
