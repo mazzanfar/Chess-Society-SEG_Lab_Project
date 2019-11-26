12edf85c560d17783594b9fffa8570572cd75eab
Sten Laane <sten.laane@kcl.ac.uk>
2019-11-26 15:58:53 +0000

    Fix broken redirect

--- public/welcome.php
+++ public/welcome.php
@@ -10,7 +10,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
+    redirect_to("log_in/login.php");
     exit;
 }
 ?>
