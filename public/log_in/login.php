036283d80ccda24d3b8c0a5c96e3556400ad380c
Sten Laane <sten.laane@kcl.ac.uk>
2019-12-03 16:26:55 +0000

    Fix redirects

--- public/log_in/login.php
+++ public/log_in/login.php
@@ -8,7 +8,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    redirect_to("/welcome.php");
     exit;
 }
 
@@ -66,7 +66,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            redirect_to("/welcome.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
