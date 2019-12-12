b57b723479d1ad33d16c418fe20c815091be20a9
Sten Laane <sten.laane@kcl.ac.uk>
2019-12-12 15:05:26 +0000

    Fix credentials in github

---
+++ private/db_dev_credentials.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', null);
+define('DB_NAME', 'chess_society_db');
+?>
--- private/db_prod_credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_SERVER', 'fdb25.awardspace.net');
-define('DB_USER', '3255002_chess');
-define('DB_PASS', "Asd123123");
-define('DB_NAME', '3255002_chess');
-?>
